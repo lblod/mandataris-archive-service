090013d38afe440b2012c6448013bdcec62952ff
Claire Lovisa <lovisa.claire@gmail.com>
2020-10-09 13:56:54 +0200

    Go through mu-auth when delete and refresh cache

--- app.js
+++ app.js
@@ -2,42 +2,57 @@ import { app, errorHandler } from 'mu';
 import bodyParser from 'body-parser';
 import flatten from 'lodash.flatten';
 import {
-  getMandataris,
-  moveMandatarisToGraveyardGraph,
+  getMandatarisTriples,
+  deleteMandataris,
+  copyMandatarisToGraveyardGraph,
   updateMandatarisType,
   addArchivingReason,
   hasDuplicate,
   moveDuplicationInfoTriplesToGraveyardGraph,
   updateMandatarisInRelatedTriples,
+  forceCacheRefresh,
   moveMandatarisRelationshipsToGraveyardGraph
 } from './lib/queries';
 
 app.use(bodyParser.json({ type: function(req) { return /^application\/json/.test(req.get('content-type')); } }));
 
 app.delete('/:uuid/archive', async function( req, res ) {
-  const mandataris = await getMandataris(req.params.uuid);
+  const mandatarisTriples = await getMandatarisTriples(req.params.uuid);
+  const mandataris = mandatarisTriples[0]['s'].value;
+
   if (!mandataris) {
     console.log(`Mandataris with uuid ${req.params.uuid} not found.`);
     return res.status(404).send();
   }
 
   try {
+    const duplicatedMandataris = await hasDuplicate(mandataris);
+
     console.log(`Archiving mandataris ${mandataris}`);
-    const result = await moveMandatarisToGraveyardGraph(mandataris);
+
+    // If the delete works, the user has the rights to delete the mandataris.
+    // We can then use sudo queries as the user has the appropriate rights on the data he's trying to process.
+    const result = await deleteMandataris(mandataris);
     if (!result) {
       console.log(`The user doesn't have the rights to delete ${mandataris}`);
     }
-    //await updateMandatarisType(mandataris);
-    //await addArchivingReason(mandataris);
-    const duplicatedMandataris = await hasDuplicate(mandataris);
+
+    await copyMandatarisToGraveyardGraph(mandatarisTriples);
+
+    await updateMandatarisType(mandataris);
+    await addArchivingReason(mandataris);
+
     if (duplicatedMandataris) {
+      console.log(`A duplicate has been found: ${duplicatedMandataris}`);
       await moveDuplicationInfoTriplesToGraveyardGraph(duplicatedMandataris)
       await updateMandatarisInRelatedTriples(mandataris);
     } else {
-      //await moveMandatarisRelationshipsToGraveyardGraph(mandataris);
+      console.log(`No duplicates, moving relationships to graveyard`);
+      await forceCacheRefresh(mandatarisTriples);
+      await moveMandatarisRelationshipsToGraveyardGraph(mandataris);
     }
   } catch (e) {
-    console.log(`Something went wrong while handling deltas for mandataris ${mandataris}`);
+    console.log(`Something went wrong while processing mandataris ${mandataris}`);
     console.log(e);
     res.status(400).send({
       errors: [{
